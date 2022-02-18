78cfbc16d0ba2c8f7684f45975531a4a1168d658
varun3867 <varun3867@gmail.com>
2022-02-18 11:42:14 +0000

    Ramesh sir

---
+++ Rameshsir/Microservices Advanced/MicroservicesOAuthWorkspace/FlightsWebSite/src/main/java/com/brownfield/pss/springsecurity/configuration/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.brownfield.pss.springsecurity.configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+ @Override
+ protected void configure(HttpSecurity http) throws Exception {
+  http.authorizeRequests()
+      .antMatchers("/login/**").permitAll()	  
+      .anyRequest().authenticated()
+      .and().formLogin()
+   		 	.loginPage("/login/form") 
+   		 	.loginProcessingUrl("/processlogin")
+   		 	.usernameParameter("username")
+   		 	.passwordParameter("password")
+   		 	.failureUrl("/login/form?error")
+   	  .and().logout()
+   	  		.logoutUrl("/logout") //internally mapped with LogoutController
+   	  		.logoutSuccessUrl("/login/form?logout")
+   	  .and().csrf().disable();
+ }
+
+ @Override
+ public void configure(AuthenticationManagerBuilder auth) throws Exception {
+  auth.inMemoryAuthentication()
+   	  .withUser("user1@example.com").password("user1").roles("USER").and()
+   	  .withUser("user2@example.com").password("user2").roles("USER");
+ }
+}
